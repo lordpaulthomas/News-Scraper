41fd4fe8f15f9e2125b50ce8e34d3825916b8681
Paul Thomas <traitorpt@gmail.com>
2019-11-05 09:30:09 -0800

    added gitignore to backend

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/news-scraper"
-);
+  process.env.MONGODB_URI || "mongodb://localhost/news-scraper", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 app.listen(PORT, function() {
   console.log(`API Server now listening on PORT ${PORT}!`)
